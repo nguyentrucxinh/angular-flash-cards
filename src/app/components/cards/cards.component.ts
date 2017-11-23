184e6f9f8c308b01c75531eee4112daffde57dfa
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-11-23 20:33:40 +0700

    Get data from api

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, RequestOptions, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html'
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit {
 
+  public cards: any[] = [];
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  public constructor(private http: Http) {
+  }
+
+  findAll(): void {
+    this.http.get(`${environment.apiUrl}/api/user/cards`)
+    .map((response: Response) => response.json())
+    .subscribe(
+      (success: any) => {
+        this.cards = success;
+      },
+      (error: any) => {
+      }
+    );
+  }
 }
