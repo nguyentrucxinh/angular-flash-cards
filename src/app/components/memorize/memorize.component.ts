fce74eff44ac890c144752dc06ea343705b3a10d
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-11-23 21:10:43 +0700

    Update memori

--- src/app/components/memorize/memorize.component.ts
+++ src/app/components/memorize/memorize.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, RequestOptions, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-memorize',
   templateUrl: './memorize.component.html'
 })
-export class MemorizeComponent {
+export class MemorizeComponent implements OnInit {
   
+  public card: any = {
+    type: false,
+    front: "",
+    back: "",
+    known: false
+  };
+
+  ngOnInit(): void {
+    this.findById(5664248772427776);
+  }
+
+  public constructor(private http: Http) {
+  }
+
+  findById(id: number): void {
+    this.http.get(`${environment.apiUrl}/api/user/cards/${id}`)
+    .map((response: Response) => response.json())
+    .subscribe(
+      (success: any) => {
+        this.card = success;
+      },
+      (error: any) => {
+      }
+    );
+  }
 }
